39836c07dddb2bf160cdd987f5caf7bc8a9183ac
cypirando <fabiocypriano@gmail.com>
2023-02-07 21:46:15 -0300

    correção de pequenos bugs

--- src/components/ConfigText/index.tsx
+++ src/components/ConfigText/index.tsx
@@ -1,3 +1,43 @@
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useSearchParams } from "react-router-dom";
+
+// interface Question {
+//   id: number;
+//   question_text: string;
+// }
+
+// const ConfigText = (props: any) => {
+
+//   function getQuestionText(array: Question[], id: any): string {
+//     return array.filter((obj) => obj.id === id)[0].question_text;
+//   }
+
+//   const [ratingConfig, setRatingConfig] = useState<Question[]>([]);
+
+//   let [searchParams] = useSearchParams();
+//   let idStr: any = searchParams.get("id");
+//   let newId: number = +idStr;
+
+//   /*Mudar o id aqui */
+//   const questionText = ratingConfig.length
+//     ? getQuestionText(ratingConfig, newId)
+//     : "";
+//   /* */
+//   useEffect(() => {
+//     axios(`http://localhost:9000/quiz`).then((dados) => {
+//       setRatingConfig(dados.data.message);
+//     });
+//   }, []);
+//   // console.log(questionText, "dddd");
+//   return (
+//     <>
+//       <p>{questionText}</p>
+//     </>
+//   );
+// };
+
+// export default ConfigText;
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
@@ -6,70 +46,53 @@ interface Question {
   id: number;
   question_text: string;
 }
-
-const ConfigText = (props: any) => {
-  
-  function getQuestionText(array: Question[], id: any): string {
-    return array.filter((obj) => obj.id === id)[0].question_text;
-  }
-  
+const ConfigText = () => {
   const [ratingConfig, setRatingConfig] = useState<Question[]>([]);
-  
-  let [searchParams] = useSearchParams();
-  let idStr: any = searchParams.get("id");
-  let newId: number = +idStr;
+  const [questionText, setQuestionText] = useState("");
+  const [searchParams] = useSearchParams();
 
-  /*Mudar o id aqui */
-  const questionText = ratingConfig.length
-    ? getQuestionText(ratingConfig, newId)
-    : "";
-  /* */
   useEffect(() => {
-    axios(`http://localhost:9000/quiz`).then((dados) => {
-      setRatingConfig(dados.data.message);
+    if (!searchParams) {
+      return;
+    }
+
+    axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
+      if (!message) return;
+      setRatingConfig(message);
+      // const id = +searchParams.get("id");
+      const idStr = searchParams.get("id");
+      const id = idStr ? +idStr : 0;
+      setQuestionText(
+        message.length
+          ? message.filter(({ id: questionId }: any) => id === questionId)[0]
+              .question_text
+          : ""
+      );
     });
-  }, []);
-  // console.log(questionText, "dddd");
-  return (
-    <>
-      <p>{questionText}</p>
-    </>
-  );
+
+    // axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
+    //   setRatingConfig(message);
+    //   const idStr = searchParams.get("id");
+    //   const id = idStr ? +idStr : 0;
+    //   setQuestionText(
+    //     message.length
+    //       ? message.filter(({ id: questionId }: any) => id === questionId)[0]
+    //           .question_text
+    //       : ""
+    //   );
+    // });
+  }, [searchParams]);
+  // useEffect(() => {
+  //   axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
+  //     setRatingConfig(message);
+  //     const id = +searchParams.get("id");
+  //     setQuestionText(
+  //       message.length ? message.filter(({ id: questionId }) => id === questionId)[0].question_text : ""
+  //     );
+  //   });
+  // }, [searchParams]);
+
+  return <p>{questionText}</p>;
 };
 
 export default ConfigText;
-
-//   import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { useParams, useSearchParams } from "react-router-dom";
-
-// // interface Question {
-// //     id: number;
-// //     question_text: string;
-// //   }
-
-// const ConfigText = (props: any) => {
-//   const params = useParams();
-//   const { id } = params;
-
-//   let [searchParams] = useSearchParams();
-
-//   console.log(searchParams.get("id"), "queryScreen");
-
-//   const [ratingConfig, setRatingConfig] = useState<any>([]);
-
-//   console.log(ratingConfig);
-//   useEffect(() => {
-//     axios(`http://localhost:9000/quiz`).then((dados) => {
-//       setRatingConfig(dados.data.message);
-//     });
-//   }, []);
-
-//   return (
-//     <>
-//       <p>{`Params:${id}`}{`QS: ${searchParams}`}</p>
-//     </>
-//   );
-// };
-
-// export default ConfigText;
