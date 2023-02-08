fd0584e87ca1b34f4543b726cc903d87afefd079
cypirando <fabiocypriano@gmail.com>
2023-02-07 21:47:39 -0300

    correção de pequenos bugs

--- src/components/ConfigText/index.tsx
+++ src/components/ConfigText/index.tsx
@@ -1,43 +1,3 @@
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import { useSearchParams } from "react-router-dom";
-
-// interface Question {
-//   id: number;
-//   question_text: string;
-// }
-
-// const ConfigText = (props: any) => {
-
-//   function getQuestionText(array: Question[], id: any): string {
-//     return array.filter((obj) => obj.id === id)[0].question_text;
-//   }
-
-//   const [ratingConfig, setRatingConfig] = useState<Question[]>([]);
-
-//   let [searchParams] = useSearchParams();
-//   let idStr: any = searchParams.get("id");
-//   let newId: number = +idStr;
-
-//   /*Mudar o id aqui */
-//   const questionText = ratingConfig.length
-//     ? getQuestionText(ratingConfig, newId)
-//     : "";
-//   /* */
-//   useEffect(() => {
-//     axios(`http://localhost:9000/quiz`).then((dados) => {
-//       setRatingConfig(dados.data.message);
-//     });
-//   }, []);
-//   // console.log(questionText, "dddd");
-//   return (
-//     <>
-//       <p>{questionText}</p>
-//     </>
-//   );
-// };
-
-// export default ConfigText;
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
@@ -59,7 +19,6 @@ const ConfigText = () => {
     axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
       if (!message) return;
       setRatingConfig(message);
-      // const id = +searchParams.get("id");
       const idStr = searchParams.get("id");
       const id = idStr ? +idStr : 0;
       setQuestionText(
@@ -69,28 +28,7 @@ const ConfigText = () => {
           : ""
       );
     });
-
-    // axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
-    //   setRatingConfig(message);
-    //   const idStr = searchParams.get("id");
-    //   const id = idStr ? +idStr : 0;
-    //   setQuestionText(
-    //     message.length
-    //       ? message.filter(({ id: questionId }: any) => id === questionId)[0]
-    //           .question_text
-    //       : ""
-    //   );
-    // });
   }, [searchParams]);
-  // useEffect(() => {
-  //   axios(`http://localhost:9000/quiz`).then(({ data: { message } }) => {
-  //     setRatingConfig(message);
-  //     const id = +searchParams.get("id");
-  //     setQuestionText(
-  //       message.length ? message.filter(({ id: questionId }) => id === questionId)[0].question_text : ""
-  //     );
-  //   });
-  // }, [searchParams]);
 
   return <p>{questionText}</p>;
 };
